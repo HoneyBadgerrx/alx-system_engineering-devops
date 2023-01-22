fbcdad7681f69f8047673a5767432e72e300b705
Kabir Atoyebi <katoyebir@gmail.com>
2023-01-22 21:56:39 +0100

    test

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,6 @@
 # Install a package using Puppet
 
-exec { 'install flask':
-  command => '/usr/bin/pip3 install flask -v 2.1.0'
-}
-
 package { 'flask':
   ensure => 'installed',
-  before => Exec['install flask']
+  command => '/usr/bin/pip3 install flask -v 2.1.0'
 }
